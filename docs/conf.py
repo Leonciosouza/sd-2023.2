4176b62f9d284e0bc03acea1a097aaf41f3aadd6
Leonciosouza <107338710+Leonciosouza@users.noreply.github.com>
2023-09-05 15:04:56 +0000

    Construção-sd

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sd-2023.2'
+copyright = '2023, Jonathan Leoncio'
+author = 'Jonathan Leoncio'
+release = '2023.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser"
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. sd-2023.2 documentation master file, created by
+   sphinx-quickstart on Tue Sep  5 14:22:25 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sd-2023.2's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
